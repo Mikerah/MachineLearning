c3a51b2bd076e577c69f68fa117becc0329faad7
Mikerah <mikerah14@gmail.com>
2017-02-06 15:58:20 -0500

    Added skeleton code to knn.py

---
+++ KNN/knn.py
@@ -0,0 +1,14 @@
+import numpy as np
+import scipy as sp
+
+class KNearestNeighbors():
+
+    def __init__(self, training_set, test_set):
+        self.training_set = training_set
+        self.test_set = test_set
+        
+    def score(self):
+        pass
+        
+    def fit(self):
+        pass
