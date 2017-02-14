cdf97f8faba3c6c7bbf6299635b287b652170b50
Mikerah <mikerah14@gmail.com>
2017-02-14 09:11:20 -0500

    added new folder for simple linear regression

---
+++ SLR/slr.py
@@ -0,0 +1,14 @@
+
+class SimpleLinearRegression():
+    """
+    Simple implementation of simple linear regression
+    """
+    
+    def __init__(self):
+        pass
+        
+    def predict(self):
+        pass
+        
+    def score(self):
+        pass
