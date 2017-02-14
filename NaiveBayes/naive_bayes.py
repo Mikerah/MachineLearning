1759ae6ff897531defd4d13492e2e42c5c845836
Mikerah <mikerah14@gmail.com>
2017-02-14 09:09:03 -0500

    Added new folder for naive bayes classifier

---
+++ NaiveBayes/naive_bayes.py
@@ -0,0 +1,14 @@
+
+class NaiveBayesClassifier():
+    """
+    Simple Implementation of the naive bayes classifier
+    """
+    
+    def __init__(self):
+        pass
+        
+    def predict(self):
+        pass
+        
+    def score(self):
+        pass
