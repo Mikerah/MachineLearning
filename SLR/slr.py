d768abcde94c8166d441afc38773dd7f0983ffb2
Mikerah <mikerah14@gmail.com>
2017-02-20 20:39:24 -0500

    Added more skeleton code to SLR.py

--- SLR/slr.py
+++ SLR/slr.py
@@ -7,6 +7,9 @@ class SimpleLinearRegression():
     def __init__(self):
         pass
         
+    def get_parameters(self):
+        pass
+        
     def predict(self):
         pass
         
