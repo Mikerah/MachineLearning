8eb82646fb637359c21bbbdd1a1aa57d949f2064
Mikerah <mikerah14@gmail.com>
2017-02-21 07:41:15 -0500

    Added private methods to linear regression

--- SLR/slr.py
+++ SLR/slr.py
@@ -4,7 +4,14 @@ class SimpleLinearRegression():
     Simple implementation of simple linear regression
     """
     
-    def __init__(self):
+    def __init__(self, train_x, train_y):
+        self.train_x = train_x
+        self.train_y = train_y
+        
+    def _covariance(self):
+        pass
+        
+    def _variance(self):
         pass
         
     def get_parameters(self):
